c37608fcd7c41d8017fb37f1e9a7476bb9af6569
Code-Master12 <121521567+Code-Master12@users.noreply.github.com>
2024-07-31 14:17:13 +0300

    Update Trime-S.lua

--- Trime-S.lua
+++ Trime-S.lua
@@ -47,7 +47,7 @@ Section:NewButton("Exit", "Exits from Trime -S", function()
     print("Trime -S: Exit made Place ID:", game.PlaceId)
 end)
 
-local Section = Main:NewSection("---------------------------- CONTROLS ----------------------------")
+local Section = Main:NewSection("--------------------------- CONTROLS ---------------------------")
 
 Section:NewLabel("Z: Toggle UI")
 
@@ -102,6 +102,27 @@ Section:NewToggle("Toggle Teleport (C)", "When activated, pressing C will telepo
             Text = "TP Disabled!";
         })
     end
+end)
+
+Section:NewSlider("Hitbox", "Set players hitbox size.", 300, 0, function(hbSize)
+    _G.HeadSize = hbSize
+    _G.Disabled = true
+    
+    game:GetService('RunService').RenderStepped:connect(function()
+    if _G.Disabled then
+    for i,v in next, game:GetService('Players'):GetPlayers() do
+    if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+    pcall(function()
+    v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
+    v.Character.HumanoidRootPart.Transparency = 0.7
+    v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue")
+    v.Character.HumanoidRootPart.Material = "Neon"
+    v.Character.HumanoidRootPart.CanCollide = false
+    end)
+    end
+    end
+    end
+    end)
 end)
 
 local function onKeyPressZ(input, gameProcessed)
